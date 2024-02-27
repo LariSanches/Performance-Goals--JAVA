1bbe67e4892e896a308cdec543e5f4a659e4f56b
Larissa Sanches <larissaesanches@gmail.com>
2024-02-27 10:06:54 -0300

    Atualzação Projeto

--- src/produto/Menu.java
+++ src/produto/Menu.java
@@ -1,13 +1,94 @@
 package produto;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
+import produto.util.Cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+
+		Scanner leia = new Scanner(System.in);
+
+		int opcao;
+
+		opcao = 0;
+
+		while (true) {
+
+			System.out.println(Cores.TEXT_RED + Cores.ANSI_WHITE_BACKGROUND
+					+ "*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println(" 	DOCE JAVA PRODUTOS PARA CONFEITARIA          ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Criar Produto                        ");
+			System.out.println("            2 - Listar todas os produtos             ");
+			System.out.println("            3 - Consultar produto por ID             ");
+			System.out.println("            4 - Atualizar produto		     ");
+			System.out.println("            5 - Deletar produto                      ");
+			System.out.println("            6 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("*****************************************************");
+			System.out.println("Digite o número da opção desejada:                   ");
+			System.out.println("                                                     " + Cores.TEXT_RESET);
+
+			try {
+				opcao = leia.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("\nDigite valores inteiros!");
+				leia.nextLine();
+				opcao = 0;
+			}
+
+			if (opcao == 6) {
+				System.out.println("\nAtendimento Doce Java finalizado!");
+				System.exit(0);
+			}
+			switch (opcao) {
+
+			case 1:
+				System.out.println("\n Criar Produto");
+				keyPress();
+				break;
+			case 2:
+				System.out.println("\n Listar todas os produtos");
+				keyPress();
+				break;
+			case 3:
+				System.out.println("\n Consultar produto por ID");
+				keyPress();
+				break;
+			case 4:
+				System.out.println("\n Atualizar dados do Produto");
+				keyPress();
+				break;
+			case 5:
+				System.out.println("\n Deletar produto");
+
+			default:
+				System.out.println("\nOpção Inválida" + Cores.TEXT_RESET);
+				keyPress();
+				break;
+
+			}
+		}
 	}
 
+	public static void keyPress() {
+
+		try {
+
+			System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para Continuar.");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("\nVocê pressionou uma tecla diferente de enter!");
+
+		}
+	}
 }
